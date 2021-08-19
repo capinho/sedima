5a4a7fd414004ce610fbb20765c4299f17e0ea89
capinho <papadiawara@hotmail.fr>
2021-08-19 20:13:06 +0000

    delete username when we create superuser

--- accounts/models.py
+++ accounts/models.py
@@ -9,17 +9,48 @@ from django.utils.translation import gettext as _
 # Create your models here.
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+# class MyAccountManager(BaseUserManager):
+#     def create_user(self, first_name, last_name, username, email, password=None):
+#         if not email:
+#             raise ValueError('User must have an email address')
+
+#         if not username:
+#             raise ValueError('User must have an username')
+
+#         user = self.model(
+#             email = self.normalize_email(email),
+#             username = username,
+#             first_name = first_name,
+#             last_name = last_name,
+#         )
+
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(self, first_name, last_name, email, username, password):
+#         user = self.create_user(
+#             email = self.normalize_email(email),
+#             username = username,
+#             password = password,
+#             first_name = first_name,
+#             last_name = last_name,
+#         )
+#         user.is_admin = True
+#         user.is_active = True
+#         user.is_staff = True
+#         user.is_superadmin = True
+#         user.save(using=self._db)
+#         return user
+
 class MyAccountManager(BaseUserManager):
-    def create_user(self, first_name, last_name, username, email, password=None):
+    def create_user(self, first_name, last_name, email, password=None):
         if not email:
             raise ValueError('User must have an email address')
 
-        if not username:
-            raise ValueError('User must have an username')
 
         user = self.model(
             email = self.normalize_email(email),
-            username = username,
             first_name = first_name,
             last_name = last_name,
         )
@@ -28,10 +59,9 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, first_name, last_name, email, username, password):
+    def create_superuser(self, first_name, last_name, email, password):
         user = self.create_user(
             email = self.normalize_email(email),
-            username = username,
             password = password,
             first_name = first_name,
             last_name = last_name,
